a81d7f21d6c9e321306f36e64636a0f71ce4bf2c
helpvits <helpvits@gmail.com>
2017-06-12 22:33:50 +0300

    Добавил модели в админку

--- Parcer/admin.py
+++ Parcer/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from Parcer.models import GroupsList, DeltaParse, SearchStory
 # Register your models here.
+
+admin.site.register([GroupsList])
+admin.site.register([DeltaParse])
+admin.site.register([SearchStory])
